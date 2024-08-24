7d98ae6ddfb8864c6f9593821861e5d6e48edce4
FERREIRA Joelson <joelson.ferreira@vinci-energies.net>
2024-08-24 22:36:28 +0100

    menu page

---
+++ src/pages/Presentation/index.js
@@ -0,0 +1,117 @@
+// @mui material components
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import Card from "@mui/material/Card";
+
+// Material Kit 2 React components
+import MKBox from "components/MKBox";
+import MKTypography from "components/MKTypography";
+import MKButton from "components/MKButton";
+
+// Material Kit 2 React examples
+import DefaultNavbar from "examples/Navbars/DefaultNavbar";
+import DefaultFooter from "examples/Footers/DefaultFooter";
+
+// About Us page sections
+import Information from "pages/LandingPages/AboutUs/sections/Information";
+import Featuring from "pages/LandingPages/AboutUs/sections/Featuring";
+import Newsletter from "pages/LandingPages/AboutUs/sections/Newsletter";
+
+// Routes
+import routes from "routes";
+import footerRoutes from "footer.routes";
+
+// Images
+import bgImage from "assets/images/menu-digital8.png";
+
+function AboutUs() {
+  return (
+    <>
+      <DefaultNavbar
+        routes={routes}
+        action={{
+          type: "external",
+          route: "https://www.creative-tim.com/product/material-kit-react",
+          label: "Escolher um Produto",
+          color: "default",
+        }}
+        transparent
+        light
+      />
+      <MKBox
+        minHeight="75vh"
+        width="100%"
+        sx={{
+          backgroundImage: ({ functions: { linearGradient, rgba }, palette: { gradients } }) =>
+            `${linearGradient(
+              rgba(gradients.dark.main, 0.6),
+              rgba(gradients.dark.state, 0.6)
+            )}, url(${bgImage})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          display: "grid",
+          placeItems: "center",
+        }}
+      >
+        <Container>
+          <Grid
+            container
+            item
+            xs={12}
+            lg={8}
+            justifyContent="center"
+            alignItems="center"
+            flexDirection="column"
+            sx={{ mx: "auto", textAlign: "center" }}
+          >
+            <MKTypography
+              variant="h1"
+              color="white"
+              mb={3}
+              sx={({ breakpoints, typography: { size } }) => ({
+                [breakpoints.down("md")]: {
+                  fontSize: size["3xl"],
+                },
+              })}
+            >
+              Pedidos automatizados com Cardápio Digital
+            </MKTypography>
+            <Card
+              sx={{
+                p: 2,
+                mt: 0,
+                mb: 4,
+                boxShadow: ({ boxShadows: { lg } }) => lg,
+              }}
+            >
+              <MKTypography variant="h6" color="black" mt={2} mb={1}>
+                Escolha o cardápio ideal para automatizar o seu atendimento
+              </MKTypography>
+              <MKButton color="info" sx={{ color: ({ palette: { white } }) => white.main }}>
+                Compre
+              </MKButton>
+            </Card>
+          </Grid>
+        </Container>
+      </MKBox>
+      <Card
+        sx={{
+          p: 2,
+          mx: { xs: 2, lg: 3 },
+          mt: -8,
+          mb: 4,
+          boxShadow: ({ boxShadows: { xxl } }) => xxl,
+        }}
+      >
+        <Information />
+        <Featuring />
+        <Newsletter />
+      </Card>
+      <MKBox pt={6} px={1} mt={6}>
+        <DefaultFooter content={footerRoutes} />
+      </MKBox>
+    </>
+  );
+}
+
+export default AboutUs;
